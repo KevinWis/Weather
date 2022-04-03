69e47d170062a16e0bd8b1d48db23c0ff98c8084
Kevin Wischneski <kevin@firstascentdesign.com>
2022-04-03 17:31:10 -0300

    Feat: Adding geolocation API call

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,8 +1,34 @@
 import Form from "../../components/form"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 const Homepage = () => {
+    const [coordinates, setCoordinates] = useState({});
+    const [error, setError] = useState(false)
+
+    useEffect(() => { getWeather() }, [coordinates])
+
+    const getCoordinates = async ({ street, city, state, zipcode }) => {
+        const streetName = street.replace(" ", "+")
+        const cityName = city.replace(" ", "+")
+        const url = `https://cors-anywhere.herokuapp.com/https://geocoding.geo.census.gov/geocoder/locations/address?street=${streetName}&city=${cityName}&state=${state}&zip=${zipcode}&benchmark=Public_AR_Census2020&format=json`
+        try {
+            const res = await axios.get(url);
+            const data = await res.data.result.addressMatches[0];
+            setCoordinates(data.coordinates)
+        }
+        catch (err) {
+            console.log(err)
+            setError(true);
+        }
+    }
+
+    const getWeather = () => {
+
+    }
+
     return <div>
-        <Form />
+        <Form getCoordinates={getCoordinates} />
     </div>
 }
 
