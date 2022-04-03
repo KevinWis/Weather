c1d9053c410d9453f12c9a79e15a628d51b83f57
Kevin Wischneski <kevin@firstascentdesign.com>
2022-04-03 16:03:19 -0300

    Feat: Adding form validators and error messages

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,18 +1,38 @@
+import { useState, useRef, useEffect } from "react"
 import states from "../../helpers/states.json"
+
 
 import { useForm } from "react-hook-form"
 const Form = () => {
-    const { register, handleSubmit } = useForm();
+    const [zipcode, setZipCode] = useState("");
+
+    const { register, handleSubmit, formState: { errors }
+    } = useForm({
+        defaultValues: {
+            street: "",
+            city: "",
+            state: "",
+            zipcode: "",
+        }
+    });
+
+    const handleSetZipcode = (evt) => {
+        const value = evt.target.value;
+        value.length > 5 ? setZipCode(value.slice(0, 5)) : setZipCode(value);
+    }
+
     return <form onSubmit={handleSubmit((data) => {
         console.log(data)
     })}>
         <div>
-            <input type="text" {...register("street")} placeholder="Street" />
-            <input type="text" {...register("city")} placeholder="City" />
+            <input type="text" {...register("street", { required: "This field is Required" })} placeholder="Street" />
+            {errors.street && <p>{errors.street.message}</p>}
+            <input type="text" {...register("city", { required: "This field is Required" })} placeholder="City" />
+            {errors.city && <p>{errors.city.message}</p>}
         </div>
 
         <div>
-            <select {...register("state")}>
+            <select {...register("state", { required: "This field is Required" })}>
                 <option value="">Select a state</option>
                 {
                     states.map((state, index) => {
@@ -21,9 +41,21 @@ const Form = () => {
                     })
                 }
             </select>
-            <input type="text"  {...register("zipcode")} placeholder="Zip code" />
+            {errors.state && <p>{errors.state.message}</p>}
+
+            <input type="number"  {...register("zipcode", {
+                required: "This is a required field",
+                minLength: { value: 5, message: "Please make sure you filled your 5-digit zip-code correctly" },
+                maxLength: { value: 5, message: "Please make sure you have 5 digits in this field" },
+            })}
+                value={zipcode}
+                onChange={handleSetZipcode}
+                placeholder="Zip code" />
+
+            {errors.zipcode && <p>{errors.zipcode.message}</p>}
         </div>
         <input type="submit" />
+
     </form>
 }
 
---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,9 @@
+import Form from "../../components/form"
+
+const Homepage = () => {
+    return <div>
+        <Form />
+    </div>
+}
+
+export default Homepage;
